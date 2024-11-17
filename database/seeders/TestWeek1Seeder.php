00ad6baa2a534f230a10b857a4812351965783a7
Minh Luu <minh.luu@pionero.io>
2024-11-17 22:03:25 +0700

    demo week1

---
+++ database/seeders/TestWeek1Seeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Facades\DeviceBind;
+use App\Facades\DeviceSingleton;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\UserRepository;
+use App\Services\DeviceService;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestWeek1Seeder extends Seeder
+{
+    // public function __construct(protected UserRepository $userRepository, protected UserRepositoryInterface $userRepositoryInterface) {}
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $device_bind = app('device_bind');
+        // $device_bind->getDeviceName();
+        // $device_bind = app('device_bind');
+        // $device_bind->getDeviceName();
+
+        // $device_singleton = app('device_singleton');
+        // $device_singleton->getDeviceName();
+        // $device_singleton = app('device_singleton');
+        // $device_singleton->getDeviceName();
+
+        // $device_instance = app('device_instance');
+        // $device_instance->getDeviceName();
+        // $device_instance = app('device_instance');
+        // $device_instance->getDeviceName();
+
+        // $this->userRepository->getUser();
+        // $this->userRepositoryInterface->getUser();
+
+        // DeviceBind::getDeviceName();
+        // DeviceSingleton::getDeviceName();
+    }
+}
